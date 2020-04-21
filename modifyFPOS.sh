9e197f1fc4c017843c97848a93b58cbd48d224a5
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2020-04-21 11:39:56 +0200

    Update modifyFPOS.sh

--- modifyFPOS.sh
+++ modifyFPOS.sh
@@ -3,6 +3,8 @@
 Disable default chromium start behaviour and go to error handling method
 sed -i '6,7s/.*/# &/' /home/pi/scripts/start_chromium_browser
 
+# Change hostname
+sudo hostnamectl set-hostname fryd-fullpageos
 
 # Edit crontabfile
 CRONTABTMPFILE=/tmp/crontab.tmp
